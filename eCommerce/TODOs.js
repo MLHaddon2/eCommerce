cce150def603d1b14949eedc2c93c631159919f7
MLHaddon2 <haddon.mike2@outlook.com>
2026-04-06 08:13:23 -0500

    Added TODOs

--- eCommerce/TODOs.js
+++ eCommerce/TODOs.js
@@ -2,3 +2,12 @@
 //       Guest carts are preserved across refreshes and merged with authenticated carts when available.
 
 // DONE: Browse page now fetches products on mount when they are not already loaded and renders a loading state.
+
+
+// TODO: Fix the issue with the cart loading forms twice. Its seems to be related to the UseEffect running twice.
+
+// TODO: Implement all the missing JPGs for the items. OR wait to have actual items after deployment.
+
+// TODO: There may be some TODO's in the adminPanel. Make sure to recreate the admin account anc check it out. For example there may still be a bug with the other users being able to access it.
+
+
