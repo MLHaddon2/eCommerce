c6e74b5c22710bb70bb89c5588024ac96af388c5
MLHaddon2 <haddon.mike2@outlook.com>
2026-04-27 20:57:52 -0500

    Checkout Changed

--- client/src/views/Main/Checkout.js
+++ client/src/views/Main/Checkout.js
@@ -79,7 +79,7 @@ function Checkout() {
 
   useEffect(() => {
       if (location) {
-          setLocationData(location);
+          setLocationData(location || location);
       };
       console.log("Location data updated:", location);
   }, [location]);
@@ -271,7 +271,7 @@ function Checkout() {
               <hr />
               <div className="d-flex justify-content-between mb-2">
                 <span>Shipping State</span>
-                <span>{locationData ? locationData : 'Loading'}</span>
+                <span>{locationData ? locationData.region : 'Loading'}</span>
               </div>
               <div className="d-flex justify-content-between mb-2">
                 <span>Subtotal:</span>
@@ -279,7 +279,7 @@ function Checkout() {
               </div>
               <div className="d-flex justify-content-between mb-2">
                 <span>
-                  Sales Tax ({((STATE_TAX_RATES[locationData] || 0) * 100).toFixed(2)}%):
+                  Sales Tax ({((STATE_TAX_RATES[locationData.region] || 0) * 100).toFixed(2)}%):
                 </span>
                 <span>${calculateSalesTax().toFixed(2)}</span>
               </div>
